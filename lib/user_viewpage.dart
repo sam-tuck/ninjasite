9a218fa035283b7c04387772c6659c1f6ba7b511
kovarny <sivochkin@gmail.com>
2023-01-25 18:35:42 +1100

    Q & A

--- lib/user_viewpage.dart
+++ lib/user_viewpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jsninja/questions/questions_page.dart';
 import 'package:jsninja/resume/user_resume_page.dart';
 import 'package:jsninja/search/search_page.dart';
 import 'package:jsninja/search/resume_page.dart';
@@ -11,7 +12,7 @@ class UserViewWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return DefaultTabController(
         initialIndex: 0,
-        length: 3,
+        length: 4,
         child: Navigator(
           onGenerateRoute: (RouteSettings settings) {
             // print('onGenerateRoute: ${settings}');
@@ -28,20 +29,24 @@ class UserViewWidget extends StatelessWidget {
               return PageRouteBuilder(
                   pageBuilder: (_, __, ___) => CoverLettersPage());
             } else {
-              //  else if (settings.name == 'lists') {
-              //   return PageRouteBuilder(
-              //       pageBuilder: (_, __, ___) => ListsPage());
-              // } else if (settings.name == 'pep admin') {
-              //   return PageRouteBuilder(
-              //       pageBuilder: (_, __, ___) => PepAdminPage());
-              // } else if (settings.name == 'pep library') {
-              //   return PageRouteBuilder(
-              //       pageBuilder: (_, __, ___) => PepLibraryPage());
-              // } else if (settings.name == 'adverse media') {
-              //   return PageRouteBuilder(
-              //       pageBuilder: (_, __, ___) => AdverseMediaPage());
-              // } else {
-              throw 'no page to show';
+              if (settings.name == 'answers') {
+                return PageRouteBuilder(
+                    pageBuilder: (_, __, ___) => QuestionsPage());
+              } else
+                //  else if (settings.name == 'lists') {
+                //   return PageRouteBuilder(
+                //       pageBuilder: (_, __, ___) => ListsPage());
+                // } else if (settings.name == 'pep admin') {
+                //   return PageRouteBuilder(
+                //       pageBuilder: (_, __, ___) => PepAdminPage());
+                // } else if (settings.name == 'pep library') {
+                //   return PageRouteBuilder(
+                //       pageBuilder: (_, __, ___) => PepLibraryPage());
+                // } else if (settings.name == 'adverse media') {
+                //   return PageRouteBuilder(
+                //       pageBuilder: (_, __, ___) => AdverseMediaPage());
+                // } else {
+                throw 'no page to show';
             }
           },
         ));
